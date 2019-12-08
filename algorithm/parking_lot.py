dd02704b94627da9088cf8e4a56e3af0d5dbc79f
lmwang27 <lmwang27@gmail.com>
2019-12-07 16:04:44 -0800

    add parking_lot.py

--- algorithm/parking_lot.py
+++ algorithm/parking_lot.py
@@ -1,3 +1,5 @@
+
+import time
 """
 Design a parking lot.
 
@@ -85,6 +87,30 @@ false
 true
 
 """
+"""
+Use cases:
+Get available count
+Park vehicle
+Clear spot
+Calculate price
+"""
+
+"""
+classes:
+ParkingLot
+Vehicles : Car,Bus,Motorcycle
+ParkingSpot
+Level
+
+"""
+
+"""
+Management类常见Use case:
+- Reservation : X
+- Serve: Park vehicle
+- Check out: Clear spot + Calculate price
+"""
+
 
 # enum type for Vehicle
 class VehicleSize:
@@ -94,12 +120,26 @@ class VehicleSize:
     other = 99
 
 
+class ParkingRate:
+    hourly_rate = 3
+    daily_rate = 10
+    half_day_rate = 8
+
+
+class Ticket:
+    def __init__(self, vehicle):
+        self.current_time = time.ctime()
+        self.end_time = 0
+        self.plate_license = vehicle.license_plate
+
+
 class Vehicle:
     def __init__(self):
         self.parking_spots = []
         self.spot_needed = 0
         self.size = None
         self.license_plate = None
+        self.ticket = 0
 
     def get_spots_needed(self):
         return self.spot_needed
@@ -122,8 +162,166 @@ class Vehicle:
 class Motorcycle(Vehicle):
     def __init__(self):
         Vehicle.__init__(self)
-        self.spot_needed =1
+        self.spot_needed = 1
         self.size = VehicleSize.Motorcycle
 
-    def can_fit_in_spot(self,spot):
+    def can_fit_in_spot(self, spot):
         return True
+
+
+class Car(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self)
+        self.spot_needed = 1
+        self.size = VehicleSize.compact
+
+    def can_fit_in_spot(self, spot):
+        return spot.get_size() == VehicleSize.large or spot.get_size == VehicleSize.compact
+
+
+class Bus(Vehicle):
+    def __init__(self):
+        Vehicle.__init__(self)
+        self.spot_needed = 5
+        self.size = VehicleSize.Large
+
+    def can_fit_in_spot(self, spot):
+        return spot.get_size() == VehicleSize.large
+
+"""
+define one spot with its property and size and 
+"""
+
+
+class ParkingSpot:
+    def __init__(self, level, row, number, size):
+        self.level = level
+        self.row = row
+        self.number = number
+        self.size = size
+        self.vehicle = None
+
+    def is_available(self):
+        if self.vehicle is None:
+            return True
+        else:
+            return False
+
+    def can_fit_vehicle(self, vehicle):
+        return self.is_available() and vehicle.can_fit_in_spot(self)
+
+    def park(self, v):
+        if not self.can_fit_vehicle(v):
+            return False
+
+        self.vehicle = v
+        self.vehicle.park_in_spot(self)
+        return True
+
+    def remove_vehicle(self):
+        self.level.spot_freed()
+        self.vehicle = None
+
+    def get_row(self):
+        return self.row
+
+    def get_spot_number(self):
+        return self.spot_number
+
+    def get_size(self):
+        return self.spot_size
+
+
+class Level:
+    def __init__(self, floor, num_rows, spots_per_row):
+        self.floor = floor
+        self.spots_per_row = spots_per_row
+        self.number_spots = 0
+        self.available_spots = 0
+        self.spots = []
+
+        for row in range(num_rows):
+            for spot1 in range(0, spots_per_row//4):
+                size = VehicleSize.motorcycle
+                self.spots.append(ParkingSpot(self, row, self.number_spots, size))
+                self.number_spots += 1
+
+            for spot2 in range(spots_per_row//4, spots_per_row//4 *3):
+                size = VehicleSize.compact
+                self.spots.append(ParkingSpot(self, row, self.number_spots, size))
+                self.number_spots += 1
+
+            for spot3 in range(spots_per_row//4 * 3, spots_per_row):
+                size = VehicleSize.large
+                self.spots.append(ParkingSpot(self, row, self.number_spots, size))
+                self.number_spots += 1
+
+        self.available_spots = self.number_spots
+
+    def park_vehicle(self, vehicle):
+        if self.get_available_spots() < vehicle.get_spots_needed():
+            return False
+        spot_num = self.find_available_spots(vehicle)
+
+        if spot_num < 0:
+            return False
+        return self.park_starting_at_spot(vehicle)
+
+    def get_available_spots(self):
+        return self.available_spots
+
+    def find_available_spots(self, vehicle):
+        spots_needed = vehicle.get_spots_needed()
+        last_row = -1
+        spots_found = 0
+
+        for i in range(len(self.spots)):
+            spot = self.spots[i]
+            if last_row != spot.get_row():
+                spots_found = 0
+                last_row = spot.get_row()
+            if spot.can_fit_vehicle(vehicle):
+                spots_found += 1
+
+            else:
+                spots_found = 0
+
+            if spots_found == spots_needed:
+                return i - (spots_needed -1)
+
+        return -1
+
+    def park_starting_at_spot(self,spot_num,vehicle):
+        vehicle.clear_spots()
+        success = True
+
+        for i in range(spot_num, spot_num+vehicle.get_spots_needed()):
+            success = success and self.spots[i].park(vehicle)
+        return success
+
+    def spot_freed(self):
+        self.available_spots +=1
+
+    def get_available_spot(self):
+        return self.available_spots
+
+
+class ParkingLot:
+    def __init__(self, n, num_rows, spots_per_row):
+        self.levels = []
+        for i in range(n):
+            self.levels.append(Level(i, num_rows, spots_per_row))
+
+    def park_vehicle(self, vehicle):
+        for level in self.levels:
+            if level.park_vehicle(vehicle):
+                return True
+        return False
+
+    def unpark_vehicle(self, vehicle):
+        vehicle.clear_spots()
+
+    def calculate_fee(self, vehicle):
+        return
+
+
